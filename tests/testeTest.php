7cd7876e8c8cba1a5032512fd8811a8bbcb37223
pedrofeijo1 <pedrofeijo1997@gmail.com>
2016-01-18 15:12:19 -0200

    add phpunit test

---
+++ tests/testeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ExampleTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $this->assertEquals(1, 1);
+    }
+
+}
